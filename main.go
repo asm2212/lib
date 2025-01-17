bd6967a756a878792cf840a7a3f5577d9043746b
asm2212 <asmareadmasu0@gmail.com>
2025-01-17 23:15:16 +0300

    Add initial project setup with Go modules and book API

--- main.go
+++ main.go
@@ -1,18 +1,47 @@
 package main
 
 import (
-	"net/http"
-	"github.com/gin-gonic/gin"
+    "net/http"
+
+    "github.com/gin-gonic/gin"
 )
 
+type book struct {
+    ID       string `json:"id"`
+    Title    string `json:"title"`
+    Author   string `json:"author"`
+    Quantity int    `json:"quantity"`
+}
+
+var books = []book{
+    {
+        ID:       "1",
+        Title:    "In Search of Lost Time",
+        Author:   "Marcel Proust",
+        Quantity: 2,
+    },
+    {
+        ID:       "2",
+        Title:    "To Kill a Mockingbird",
+        Author:   "Harper Lee",
+        Quantity: 3,
+    },
+    {
+        ID:       "3",
+        Title:    "1984",
+        Author:   "George Orwell",
+        Quantity: 1,
+    },
+}
+
+func getBooks(c *gin.Context) {
+    c.IndentedJSON(http.StatusOK, books)
+}
+
 func main() {
-	r := gin.Default()
+    router := gin.Default()
 
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{
-			"message": "pong",
-		})
-	})
+    router.GET("/books", getBooks)
 
-	r.Run() // listen and serve on 0.0.0.0:8080
+    router.Run("localhost:8080")
 }
